40c666d187e34db1e55685b3810285e3b8c3b9a0
anoushkabroal <anoushkabroal@gmail.com>
2023-11-13 14:01:13 -0500

    Implementing Callable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
+import java.util.concurrent.Callable;
+
+public class Main implements Callable<Integer> {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
     }
 }
